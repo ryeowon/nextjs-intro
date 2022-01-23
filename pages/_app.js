7b8c2fc083ec4dbd114d1375cb9f74b13baff59f
Ryeowon Hong <ryeowon13@gmail.com>
2022-01-23 22:56:39 +0900

    title and css

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css"; //_app.js 에서만 import 가능, 다른 곳에서는 module이어야 함
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
